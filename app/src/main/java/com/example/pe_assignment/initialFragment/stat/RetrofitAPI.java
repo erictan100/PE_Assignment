7166382e7090e6019a103c3f8cd0e24c04150fe7
erictan100 <SWE1904863@xmu.edu.my>
2022-07-07 14:34:10 +0800

    Eric's part

--- app/src/main/java/com/example/pe_assignment/initialFragment/stat/RetrofitAPI.java
+++ app/src/main/java/com/example/pe_assignment/initialFragment/stat/RetrofitAPI.java
@@ -8,7 +8,7 @@ import retrofit2.http.Header;
 import retrofit2.http.Headers;
 
 public interface RetrofitAPI {
-    @Headers({"X-RapidAPI-Key: 79ffa5cf02msh6527d22af231364p14cbbcjsncdb0925c279e","X-RapidAPI-Host: malaysia-covid19-data-live.p.rapidapi.com"})
+    @Headers({"X-RapidAPI-Key: 908e5176acmshc0c79425c5e762bp11b927jsn358c73371654","X-RapidAPI-Host: malaysia-covid19-data-live.p.rapidapi.com"})
     @GET("2022-05-20") //"https://malaysia-covid19-data-live.p.rapidapi.com/covid/getAllData/"
     Call<Statistics> getStatistics();
 //    @GET("all") //https://disease.sh/v3/covid-19/all
--- app/src/main/java/com/example/pe_assignment/initialFragment/stat/RetrofitAPI2.java
+++ app/src/main/java/com/example/pe_assignment/initialFragment/stat/RetrofitAPI2.java
@@ -6,7 +6,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface RetrofitAPI2 {
-    @Headers({"X-RapidAPI-Key: 79ffa5cf02msh6527d22af231364p14cbbcjsncdb0925c279e","X-RapidAPI-Host: malaysia-covid19-data-live.p.rapidapi.com"})
+    @Headers({"X-RapidAPI-Key: 908e5176acmshc0c79425c5e762bp11b927jsn358c73371654","X-RapidAPI-Host: malaysia-covid19-data-live.p.rapidapi.com"})
     @GET("total?date=2022-05-20")
     Call<Statistics2> getStatistics2();
 }
